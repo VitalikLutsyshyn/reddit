678e908900e18ead4f71d196799226771edb5e40
Vitalik <vitalii.lutsyshyn.liceum2@gmail.com>
2025-02-21 16:31:37 +0200

    update

--- models.py
+++ models.py
@@ -14,4 +14,28 @@ class User(db.Model):
     def __repr__(self):#представлення обєкта в програмі
         return f'User {self.nickname}'
     
+
+class Topics(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.Text,unique=True,nullable=True)
+    posts = db.Column(db.Text,nallable=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200),nullable=True)
+    story = db.Column(db.Text,nullable=True)
+    user_nickname = db.Column(db.String(100),unique=True,nullable=True)
+    published_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    likes = db.Column(db.Integer,nullable=True)
+    rules = db.Column(db.String)
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    comment = db.Column(db.Text,nullabele=True)
+    likes = db.Column(db.Integer,nullable=True)
+    user_nickname = db.Column(db.String,nullable=True)
+    published_at = db.Column(db.DataTime(timezone=True), default=datetime.utcnow)
+
 #таблиці на дз Subreddit,Post,Comment
