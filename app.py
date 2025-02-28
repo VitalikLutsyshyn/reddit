73d42bfe4e7e04995a08bb70097b484995aabdf1
Vitalik <vitalii.lutsyshyn.liceum2@gmail.com>
2025-02-28 16:27:22 +0200

    somethig

--- app.py
+++ app.py
@@ -34,11 +34,17 @@ def add_user():
 
 @app.route("/add_topic")
 def add_topic():
-    topic = Topic(name="Dogs")
-    topic = Topic(name="Cars")
     topic = Topic(name="Games")
+    db.session.add(topic)#Додаємо користувача в сесію
+    db.session.commit()#Збереження(додавання) у базу даних
     return f"User {topic.name} Додано!!!"
 
+@app.route("/add_post")
+def add_post():
+    post = Topic(title="I've been stuck in an elevator for over an hour",content="Today I got stuck in the elevator and was there for 2 hours. When I got stuck, I was supposed to go out with my friends, but instead I sat in the elevator all that time.")
+    db.session.add(post)#Додаємо користувача в сесію
+    db.session.commit()#Збереження(додавання) у базу даних
+    return f"User {post.name} Додано!!!"
 
     
 
